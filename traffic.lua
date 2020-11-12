288c69880ef7fc01085de493eedc27b04cbbf70d
Tinky <48660658+Tinky124@users.noreply.github.com>
2020-11-12 12:06:27 +0100

    Out of loop...

--- traffic.lua
+++ traffic.lua
@@ -2,15 +2,15 @@ Citizen.CreateThread(function()
 	for i = 1, 15 do
 		EnableDispatchService(i, EnableDispatch)
 	end
+		SetRandomBoats(EnableBoats)
+		SetRandomTrains(EnableTrains)
+                SetGarbageTrucks(EnableGarbageTrucks)
 	while true do
 		SetVehicleDensityMultiplierThisFrame((TrafficAmount/100)+.0)
 		SetPedDensityMultiplierThisFrame((PedestrianAmount/100)+.0)
 		SetRandomVehicleDensityMultiplierThisFrame((TrafficAmount/100)+.0)
 		SetParkedVehicleDensityMultiplierThisFrame((ParkedAmount/100)+.0)
 		SetScenarioPedDensityMultiplierThisFrame((PedestrianAmount/100)+.0, (PedestrianAmount/100)+.0)
-		SetRandomBoats(EnableBoats)
-		SetRandomTrains(EnableTrains)
-                SetGarbageTrucks(EnableGarbageTrucks)
 		Wait(0) -- These natives has to be called every frame.
 	end
 end)
