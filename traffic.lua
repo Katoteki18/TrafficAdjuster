5d7247555fc3797e84c67e1a0ded5a6f671cc6a9
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-07-12 18:41:27 +0100

    0.00ms :)

--- traffic.lua
+++ traffic.lua
@@ -1,16 +1,14 @@
-Citizen.CreateThread(function()
-	for i = 1, 15 do
+CreateThread(function()
+	 for i = 1, 15 do
 		EnableDispatchService(i, EnableDispatch)
-	end
-		SetRandomBoats(EnableBoats)
-		SetRandomTrains(EnableTrains)
-                SetGarbageTrucks(EnableGarbageTrucks)
-	while true do
-		SetVehicleDensityMultiplierThisFrame((TrafficAmount/100)+.0)
-		SetPedDensityMultiplierThisFrame((PedestrianAmount/100)+.0)
-		SetRandomVehicleDensityMultiplierThisFrame((TrafficAmount/100)+.0)
-		SetParkedVehicleDensityMultiplierThisFrame((ParkedAmount/100)+.0)
-		SetScenarioPedDensityMultiplierThisFrame((PedestrianAmount/100)+.0, (PedestrianAmount/100)+.0)
-		Wait(0) -- These natives has to be called every frame.
-	end
+	 end
+	 SetRandomBoats(EnableBoats)
+	 SetRandomTrains(EnableTrains)
+         SetGarbageTrucks(EnableGarbageTrucks)
+	 SetCreateRandomCops(UkljuciPoliciju)
+    	 SetCreateRandomCopsNotOnScenarios(UkljuciPoliciju)
+         SetCreateRandomCopsOnScenarios(UkljuciPoliciju)
+         SetDispatchCopsForPlayer(PlayerId(), UkljuciPoliciju)
+         SetPedPopulationBudget(0)
+         SetVehiclePopulationBudget(0)
 end)
